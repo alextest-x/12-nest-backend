47ac560b04d37500483b24a0ea6838fac4ed8f85
alextest-x <alexsnet01@gmail.com>
2023-11-10 01:49:43 -0600

    nest-backend

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+
+ // habilitamos el cors
+ //en caso de error al hacer la peticion desde el backend
+  app.enableCors();
+
   //es una restriccion tiene que enviar la informacion al servidor como quiere sino no la acepta
   app.useGlobalPipes(
     new ValidationPipe({
